db7c46d432a34e6374cde8371d198a80a8876b9b
Florian PASQUEREAU <fpasquer@e1r5p12.42.fr>
2017-05-14 19:56:59 +0200

    commit one

---
+++ Colleen.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+commentaire1
+*/
+int main(){
+/*
+commentaire2
+*/
+char*a="#include <stdio.h>%c/*%ccommentaire1%c*/%cint main(){%c/*%ccommentaire2%c*/%cchar*a=%c%s%c;printf(a, 10, 10, 10, 10, 10, 10, 10, 10, 34, a, 34, 10);return 0;}%c";printf(a, 10, 10, 10, 10, 10, 10, 10, 10, 34, a, 34, 10);return 0;}
---
+++ Sully.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+#define STR(s) char *str=#s; s
+int i=5;
+STR(
+	int main(int argc, char **argv)
+	{
+		char name[200];
+		FILE *f = NULL;
+		if(!i)
+			return 0;
+		if (strcmp(argv[0], "./Sully"))
+		{
+			printf("%s\n", argv[0]);
+			i--;
+		}
+			bzero(name,sizeof(name));
+		if (!snprintf(name,sizeof(name),"Sully_%d.c",i)||!(f=fopen(name,"w+")))
+			return 0;
+		if(!fprintf(f, "#include<stdio.h>\n#include<string.h>\n#define STR(s) char *str=#s; s\nint i=%d;\nSTR(%s)\n", i, str))
+			printf("Erreur\n");
+		fclose(f);
+		return(0);
+	}
+)

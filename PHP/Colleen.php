e90fb69bf0f64465fe960890fc5e49a91abd93ef
fpasquer <fpasquer@student.42.fr>
2017-05-16 08:54:20 +0200

    PHP/Colleen.php : done

--- PHP/Colleen.php
+++ PHP/Colleen.php
@@ -1,2 +1,13 @@
 #!/usr/bin/php
-<?php printf("fdhjkghfkghjsfkg");?>
+<?php
+/*
+commentaire1
+*/
+function main()
+{
+/*
+commentaire2
+*/
+}
+$a='#!/usr/bin/php%c<?php%c/*%ccommentaire1%c*/%cfunction main()%c{%c/*%ccommentaire2%c*/%c}%c%ca=%c%s%c;printf($a,10,10,10,10,10,10,10,10,10,10,10,36,39,$a,39,10);%c?>';printf($a,10,10,10,10,10,10,10,10,10,10,10,36,39,$a,39,10);
+?>

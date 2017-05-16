50ccbb37c3459387e90e1666638f039af9f43af0
fpasquer <fpasquer@student.42.fr>
2017-05-16 09:58:52 +0200

    Grace.c : Use define to call the main function

--- Grace.c
+++ Grace.c
@@ -4,5 +4,5 @@
 */
 #define NAME "Grace_kid.c"
 #define STR(s) char *str=#s; s
-#define MODE "w+"
-STR(int main(){FILE* f=fopen(NAME, MODE);fprintf(f,"#include <stdio.h>\n/*\n\tcommentaire\n*/\n#define NAME \"Grace_kid.c\"\n#define STR(s) char *str=#s; s\n#define MODE \"w+\"\nSTR(%s)\n", str); fclose(f);return 0;})
+#define MAIN(source) int main(){source}
+MAIN(STR(FILE* f=fopen(NAME, "w+");fprintf(f,"#include <stdio.h>\n/*\n\tcommentaire\n*/\n#define NAME \"Grace_kid.c\"\n#define STR(s) char *str=#s; s\n#define MAIN(source) int main(){source}\nMAIN(STR(%s))\n", str); fclose(f);return 0;))

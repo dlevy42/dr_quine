404d29a063e4e52816b048233056ed47aa1059dc
Florian PASQUEREAU <fpasquer@e2r4p7.42.fr>
2017-06-07 13:33:27 +0200

    fixed -1

--- Grace.c
+++ Grace.c
@@ -5,4 +5,4 @@
 #define NAME "Grace_kid.c"
 #define STR(s) char *str=#s; s
 #define MAIN(source) int main(){source}
-MAIN(STR(FILE* f=fopen(NAME, "w+");fprintf(f,"#include <stdio.h>\n/*\n\tcommentaire\n*/\n#define NAME \"Grace_kid.c\"\n#define STR(s) char *str=#s; s\n#define MAIN(source) int main(){source}\nMAIN(STR(%s))\n", str); fclose(f);return 0;))
+MAIN(STR(FILE* f=fopen(NAME, "w+");if (f==NULL)return 0;fprintf(f,"#include <stdio.h>\n/*\n\tcommentaire\n*/\n#define NAME \"Grace_kid.c\"\n#define STR(s) char *str=#s; s\n#define MAIN(source) int main(){source}\nMAIN(STR(%s))\n", str); fclose(f);return 0;))
